73c6f23723b8d82c7f810d27f96c980833695435
Ben Limmer <ben@benlimmer.com>
2015-07-20 15:24:03 -0600

    {{link-to}} is fixed.

--- tests/integration/components/dumb-link-test.js
+++ tests/integration/components/dumb-link-test.js
@@ -7,21 +7,7 @@ moduleForComponent('dumb-link', 'Integration | Component | dumb link', {
 });
 
 test('it renders', function(assert) {
-  assert.expect(2);
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
   this.render(hbs`{{dumb-link}}`);
 
-  assert.equal(this.$().text(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#dumb-link}}
-      template block text
-    {{/dumb-link}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim(), 'Click Me');
 });
